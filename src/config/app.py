92dd8d8e78968fee89dad5d3c236e1fb32b2b880
Andrii Bogdanovych <a@bogdanovych.org>
2026-05-29 15:55:41 +0300

    first commit

---
+++ src/config/app.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import os
+from importlib.metadata import metadata
+from pathlib import Path
+
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+meta = metadata("crimea-rag")
+
+
+class Settings(BaseSettings):
+    @staticmethod
+    def get_asset_dir() -> Path:
+        """Повертає шлях до директорії з ресурсами."""
+        default_assets_dir = Path(__file__).resolve().parent.parent / "assets"
+        return Path(os.environ.get("FLET_ASSETS_DIR", default_assets_dir)).resolve()
+
+    name: str = meta["name"]
+    version: str = meta["version"]
+    license: str = meta["License-Expression"]
+
+    base_url: str = ""
+
+    assets_dir: Path = get_asset_dir()
+    env_file: Path = assets_dir / ".env"
+
+    base_dir: Path = Path(__file__).resolve().parent.parent.parent
+
+    database_dir_name: str = "crimea-rag"
+
+    @computed_field
+    @property
+    def database_dir(self) -> Path:
+        return self.base_dir / "database" / self.database_dir_name
+
+    parent_dir: Path = base_dir.parent
+
+    www_project_name: str = "crimea-is-ukraine"
+    www_content_dir_name: str = "content"
+
+    @computed_field
+    @property
+    def content_dir(self) -> Path:
+        return self.parent_dir / self.www_project_name / self.www_content_dir_name
+
+    llm_model: str = "mxbai-embed-large"
+    llm_url: str = "http://crimea"
+    llm_port: int = 11434
+
+    batch_size: int = 50
+
+    model_config = SettingsConfigDict(
+        env_file=env_file,
+        env_prefix="APP__",
+        extra="ignore",
+    )
+
+
+settings = Settings()
+
+if __name__ == "__main__":
+    print(settings)
---
+++ src/migrations/create_vector_db_by_md.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import MarkdownTextSplitter
+
+from config import app
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+
+    logger.info("Завантаження Markdown файлів...")
+    documents = []
+    for path in app.settings.content_dir.rglob("**/*.md"):
+        if path.is_file():
+            try:
+                text = path.read_text(encoding="utf-8")
+                documents.append(
+                    Document(page_content=text, metadata={"source": str(path)})
+                )
+            except Exception as e:
+                logger.exception(f"Помилка читання файлу {path}: {e}")
+
+    logger.info("Розбиття текстів на чанки...")
+    text_splitter = MarkdownTextSplitter(chunk_size=500, chunk_overlap=200)
+    chunks = text_splitter.split_documents(documents)
+
+    logger.info("Зв'язок з Ollama та створення векторів...")
+    embeddings = OllamaEmbeddings(
+        model=app.settings.llm_model,
+        base_url=f"{app.settings.llm_url}:{app.settings.llm_port}",
+        mirostat=0,
+        mirostat_eta=0.0,
+        mirostat_tau=0.0,
+        tfs_z=1.0,
+    )
+
+    logger.info("Ініціалізація бази Chroma...")
+    vector_db = Chroma(
+        embedding_function=embeddings, persist_directory=str(app.settings.database_dir)
+    )
+
+    logger.info(f"Всього чанків для обробки: {len(chunks)}")
+
+    batch_size = app.settings.batch_size
+
+    logger.info(f"Запис у базу порціями по {batch_size} батчів...")
+
+    for i in range(0, len(chunks), batch_size):
+        batch = chunks[i : i + batch_size]
+        vector_db.add_documents(batch)
+        logger.info(f"Оброблено чанків: {i + len(batch)} з {len(chunks)}")
+
+    logger.info(
+        f"Успішно! База знань збережена локально в: {app.settings.database_dir}"
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    )
+
+    main()
