eb5a82749ec636ee4306f9fe5cf0d71a926a051a
Andrii Bogdanovych <a@bogdanovych.org>
2026-05-30 16:29:02 +0300

    Виправлення в ChromaManager

--- src/core/chroma_manager.py
+++ src/core/chroma_manager.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
     from langchain_core.documents.base import Document
 
 
@@ -14,33 +16,29 @@ from langchain_ollama import OllamaEmbeddings
 
 class ChromaManager:
     def __init__(
-        self, model: str, base_url: str, persist_directory: str, default_k: int
+        self, model: str, base_url: str, persist_directory: Path, k: int
     ) -> None:
 
         self.model = model
         self.base_url = base_url
-        self.persist_directory = persist_directory
-        self.k = default_k
+        self.persist_directory = str(persist_directory)
+        self.k = k
 
         self.embedding_function = OllamaEmbeddings(
-            model=app.settings.embed_model,
-            base_url=f"{app.settings.embed_url}:{app.settings.embed_port}",
+            model=self.model,
+            base_url=self.base_url,
         )
 
         self.vector_db = Chroma(
-            persist_directory=str(app.settings.database_dir),
+            persist_directory=self.persist_directory,
             embedding_function=self.embedding_function,
         )
 
-    def similarity_search(self, query: str, k: int | None = None) -> list[Document]:
-        k = self.k if k is None else k
-        return self.vector_db.similarity_search(query, k=k)
+    def similarity_search(self, query: str) -> list[Document]:
+        return self.vector_db.similarity_search(query, k=self.k)
 
-    def max_marginal_relevance_search(
-        self, query: str, k: int | None = None
-    ) -> list[Document]:
-        k = self.k if k is None else k
-        return self.vector_db.max_marginal_relevance_search(query, k=k)
+    def max_marginal_relevance_search(self, query: str) -> list[Document]:
+        return self.vector_db.max_marginal_relevance_search(query, k=self.k)
 
 
 if __name__ == "__main__":
@@ -48,9 +46,9 @@ if __name__ == "__main__":
 
     client = ChromaManager(
         model=app.settings.embed_model,
-        base_url=app.settings.embed_url,
+        base_url=f"{app.settings.embed_url}:{app.settings.embed_port}",
         persist_directory=app.settings.database_dir,
-        default_k=3,
+        k=3,
     )
 
     q = "Крим чи Рим?"
