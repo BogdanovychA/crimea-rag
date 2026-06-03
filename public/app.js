737891288e32904263bd8664ce200babc0481063
Andrii Bogdanovych <a@bogdanovych.org>
2026-06-03 08:52:20 +0300

    Додавання аналітики

---
+++ public/app.js
@@ -0,0 +1,10 @@
+let gaScript = document.createElement('script');
+gaScript.async = true;
+gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-QSDSDSB7G2';
+document.head.appendChild(gaScript);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-QSDSDSB7G2');
